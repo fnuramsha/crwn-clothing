951e3d87455edbb6ab9b79489a99260d936fcf4d
Ramsha <fnuramsha@gmail.com>
2024-06-28 13:55:45 +0200

    optimized code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import React from "react";
 const App = () => {
-  return <div>Hello world</div>;
+  const categories = [
+    {
+      id: 1,
+      title: "HATS",
+    },
+    {
+      id: 1,
+      title: "JACKETS",
+    },
+    {
+      id: 1,
+      title: "SNEAKERS",
+    },
+    {
+      id: 1,
+      title: "WOMENS",
+    },
+    {
+      id: 1,
+      title: "MENS",
+    },
+  ];
+  return (
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          {/* {img} */}
+          <div className="category-body-container">
+            <h2> {title} </h2>
+            <p> SHOP NOW </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
